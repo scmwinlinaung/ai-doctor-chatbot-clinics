383b54e9b305c0712a298a2e4ae31cc96c9db199
winlinaung <winlinaung@winlinaungs-MacBook-Air.local>
2025-11-19 21:17:09 +0630

    update HiveDataKeys

--- lib/core/config/hive_data_keys.dart
+++ lib/core/config/hive_data_keys.dart
@@ -1,5 +1,5 @@
 class HiveDataKeys {
-  static const String TOKEN = "TOKEN";
-  static const String AUTH_KEYS = "AUTH_KEYS";
-  static const String authToken = "auth_token";
+  static const String TOKEN = "CLINICS_TOKEN";
+  static const String AUTH_KEYS = "CLINICS_AUTH_KEYS";
+  static const String authToken = "CLINICS_AUTH_TOKEN";
 }
